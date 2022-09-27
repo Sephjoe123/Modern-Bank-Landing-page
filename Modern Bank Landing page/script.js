2f3c2acbed296b3a97ade75c15b9ce564050972a
Bello Joseph <joexef87@gmail.com>
2022-09-27 23:16:43 +0100

    Update script.js

--- Modern Bank Landing page/script.js
+++ Modern Bank Landing page/script.js
@@ -18,7 +18,7 @@ function animateOnScroll (){
      for( let i = 0; i < reveal.length; i++){
         let scrollHeight = window.innerHeight;
         let scrollTop = reveal[i].getBoundingClientRect().top;
-        let revealPoint = 100;
+        let revealPoint = 200;
         if(scrollTop < scrollHeight - revealPoint){
            reveal[i].classList.add("show");
         }
