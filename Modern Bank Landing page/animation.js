8a66140ea73d3b7e8e84df4a9be47edf89a0d8c1
Bello Joseph <joexef87@gmail.com>
2023-10-18 17:30:29 +0100

    Update animation.js

--- Modern Bank Landing page/animation.js
+++ Modern Bank Landing page/animation.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let animatedText = document.querySelector(".discount-price")
     
     var options = {
-      strings: ["Enticing rates","24/7 Protection","Great deals","20% Discount"],
+      strings: ["24/7 Protection","Great deals","20% Discount"],
       typeSpeed: 100,
       loop: true, 
     };
@@ -15,41 +15,33 @@ document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed(animatedText, options);
     })
 
-    const timeline = gsap.timeline()
+gsap.defaults({ duration: 0.8, ease:"easeInOut", }); 
 
-    timeline.from(heroImg,{
+const timeline = gsap.timeline();
+
+timeline.from(heroImg, {
     x: 500,
-    duration: 2,
-    ease: "easeInOut",
-    opacity: 0, 
-    })
+    opacity: 0,
+});
 
-    gsap.from(discount,{
+timeline.from(discount, {
     y: -500,
-    duration: 1,
-    ease: "easeInOut",
-    opacity: 0, 
-    delay: 1,
-    })
+    opacity: 0,
+});
 
-    gsap.from(discountP,{
-     y: 500,
-     duration: 2,
-     ease : "easeInOut",
-     opacity: 0,
-    })
+timeline.from(discountP, {
+    y: 500,
+    opacity: 0,
+});
 
-    timeline.from("#home h1",{
-     x: -1000,
-     duration: 1,
-     ease: "easeInOut",
-     opacity: 0,
-     
-    })
+timeline.from("#home h1", {
+    x: -1000,
+    opacity: 0,
+});
 
     
     const moveAnimation = gsap.from(stats.children, {
-        y: 500,
+        y: 300,
         opacity: 0,
         duration: 1.5,
         ease: "easeInOut",
@@ -57,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     
 
-let scrollLength = 470;
+let scrollLength = 400;
 
 function triggerScroll() {
     console.log(window.scrollY)
@@ -70,7 +62,6 @@ function triggerScroll() {
     }
   }
   
-
     window.addEventListener("scroll", triggerScroll);
     triggerScroll()
   
